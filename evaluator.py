9c1b0f9c3cb2a50b0f3f5b17b2ae47ec61593891
julialu-7 <147670672+julialu-7@users.noreply.github.com>
2025-04-27 17:35:49 -0400

    revert evaluator.py

--- evaluator.py
+++ evaluator.py
@@ -111,11 +111,12 @@ class ProbingEvaluator:
         for epoch in tqdm(range(epochs), desc=f"Probe prediction epochs"):
             for batch in tqdm(dataset, desc="Probe prediction step"):
                 ################################################################################
-                # Forward pass through the JEPA model
-                states = batch.states.to(self.device)         # [B, T, C, H, W]
-                actions = batch.actions.to(self.device)       # [B, T-1, action_dim]
-                pred_encs = model(states=states, actions=actions)  # [B, T, D]
-                pred_encs = pred_encs.transpose(0, 1)        # [T, B, D]
+                # TODO: Forward pass through your model
+                init_states = batch.states[:, 0:1]  # BS, 1, C, H, W
+                pred_encs = model(states=init_states, actions=batch.actions)
+                pred_encs = pred_encs.transpose(0, 1)  # # BS, T, D --> T, BS, D
+
+                # Make sure pred_encs has shape (T, BS, D) at this point
                 ################################################################################
 
                 pred_encs = pred_encs.detach()
@@ -208,11 +209,13 @@ class ProbingEvaluator:
 
         for idx, batch in enumerate(tqdm(val_ds, desc="Eval probe pred")):
             ################################################################################
-            # Forward pass through the JEPA model
-            states = batch.states.to(self.device)         # [B, T, C, H, W]
-            actions = batch.actions.to(self.device)       # [B, T-1, action_dim]
-            pred_encs = model(states=states, actions=actions)  # [B, T, D]
-            pred_encs = pred_encs.transpose(0, 1)        # [T, B, D]
+            # TODO: Forward pass through your model
+            init_states = batch.states[:, 0:1]  # BS, 1 C, H, W
+            pred_encs = model(states=init_states, actions=batch.actions)
+            # # BS, T, D --> T, BS, D
+            pred_encs = pred_encs.transpose(0, 1)
+
+            # Make sure pred_encs has shape (T, BS, D) at this point
             ################################################################################
 
             target = getattr(batch, "locations").cuda()
